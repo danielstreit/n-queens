9a54762a0837cd5814fda1646b398b87e066c305
danielstreit <daniel.streit@gmail.com>
2014-07-30 15:53:55 -0700

    working solution up to 4

--- src/solvers.js
+++ src/solvers.js
@@ -14,20 +14,64 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n: n});
+  for (var i = 0; i < n; i++) {
+    solution.attributes[i][i] = 1;
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.attributes;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var perms = [];
+  var matrices = [];
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  var permutate = function() {
+    var choices = [0, 1];
+    var outcomes = [];
+    var thusFar = [];
+
+    var combos = function(roundsToGo) {
+      if (roundsToGo === 0) {
+        outcomes.push(thusFar.slice());
+        return;
+      }
+      for (var i = 0; i < choices.length; i++) {
+        thusFar.push(choices[i]);
+        combos(roundsToGo - 1);
+        thusFar.pop();
+      }
+    }
+    combos(n*n);
+    return outcomes;
+
+  };
+
+  perms = permutate();
+  perms = perms.filter(function(el) {
+      return el.reduce(function(a, b) {
+        return a + b;
+      }) === n;
+    });
+  perms.forEach(function(el) {
+    var temp = [];
+    while (el.length) {
+      temp.push(el.splice(0, n));
+    }
+    matrices.push(temp);
+  });
+
+  matrices = matrices.filter(function(matrix) {
+    var board = new Board(matrix);
+    return !board.hasAnyRooksConflicts();
+  });
+
+  console.log('Number of solutions for ' + n + ' rooks:', matrices.length);
+  return matrices.length;
 };
 
 
