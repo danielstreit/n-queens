4f6d289fa1293b886987e106427b706883680c8f
danielstreit <daniel.streit@gmail.com>
2014-07-30 21:15:12 -0700

    Implement nQueens

--- src/solvers.js
+++ src/solvers.js
@@ -39,7 +39,6 @@ window.countNRooksSolutions = function(n) {
   var inner = function inner(arr, solution) {
     var holder;
     var solution = solution || [];
-    parent = parent || null;
     if (arr.length === 1) {
       solution.push(arr[0]);
       answers.push(solution);
@@ -59,8 +58,7 @@ window.countNRooksSolutions = function(n) {
     }
   }
   inner(matrix);
-  console.log(answers)
-
+ // console.log(answers)
 
   console.log('Number of solutions for ' + n + ' rooks:', answers.length);
   return answers.length;
@@ -71,17 +69,114 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var matrix = [];
+
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      matrix.push([i, j]);
+    }
+  }
+
+  var answers = [];
+  var inner = function inner(arr, solution) {
+    var holder;
+    var solution = solution || [];
+    parent = parent || null;
+    if (arr.length === 1) {
+      solution.push(arr[0]);
+      answers.push(solution);
+    } else {
+      for (var i = 0; i < Math.sqrt(arr.length); i++) {
+        solution.push(arr[i]);
+        holder = arr.filter(function(el) {
+          if (el[0] === arr[i][0] ||
+              el[1] === arr[i][1] ||
+              Math.abs(arr[i][0] - el[0]) === Math.abs(arr[i][1] - el[1])) {
+            return false;
+          }
+          return true;
+        });
+        inner(holder, solution.slice());
+        solution.pop();
+      }
+    }
+  }
+
+  inner(matrix);
+  answers = answers.filter(function(item) {
+    return item.length === n;
+  });
+
+  console.log(answers[0]);
+
+  var board = new Board({n: n});
+  if (answers.length > 0) {
+    answers[0].forEach(function(point) {
+      board.togglePiece(point[0], point[1]);
+    });
+  }
+
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(board.attributes));
+  return board.attributes;
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  if (n === 0) { return 1; }
+  var matrix = [];
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      matrix.push([i, j]);
+    }
+  }
+
+  var answers = [];
+  var inner = function inner(arr, solution) {
+    var holder;
+    var solution = solution || [];
+    parent = parent || null;
+    if (arr.length === 1) {
+      solution.push(arr[0]);
+      answers.push(solution);
+    } else {
+      for (var i = 0; i < Math.sqrt(arr.length); i++) {
+        solution.push(arr[i]);
+        holder = arr.filter(function(el) {
+          if (el[0] === arr[i][0] ||
+              el[1] === arr[i][1] ||
+              Math.abs(arr[i][0] - el[0]) === Math.abs(arr[i][1] - el[1])) {
+            return false;
+          }
+          return true;
+        });
+        inner(holder, solution.slice());
+        solution.pop();
+      }
+    }
+  }
+
+  inner(matrix);
+  answers = answers.filter(function(item) {
+    return item.length === n;
+  });
+  var temp = [];
+  answers.forEach(function(answer) {
+    temp.push(stringifyAndSortAnswer(answer));
+  });
+
+  answers = _.unique(temp);
+
+  console.log('Number of solutions for ' + n + ' queens:', answers.length);
+  return answers.length;
 };
+
+var stringifyAndSortAnswer = function(a) {
+  var result = [];
+  a.forEach(function(point) {
+    result.push('' + point[0] + point[1]);
+  });
+  return result.sort().join();
+}
